4a6b35681c755ba982fe0f18eaf38334c7e0e6b9
Danielle Gabriszeski <dfleharty88@gmail.com>
2021-03-27 15:31:56 -0400

    updates

--- app.py
+++ app.py
@@ -1,29 +1,131 @@
-from flask import Flask, render_template, redirect, url_for, g
+from flask import (Flask, render_template, redirect, 
+                    url_for, g, flash, abort)
+from flask_login import (current_user, LoginManager, 
+                        login_user, logout_user, login_required)
+from flask_bcrypt import generate_password_hash, check_password_hash
 
 import models
+import forms
 
 
 app = Flask(__name__)
-app.run(debug=True, port=8000, host='0.0.0.0')
+app.secret_key = '#(SJ(DGKncn9ee#$#@dkduedj)%K'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+
+@app.before_request
+def before_request():
+    g.db = models.db
+    g.db.connect()
+    g.user = current_user
+
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash('You have registered successfully!', 'success')
+        models.User.create_user(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.email == form.email.data)
+            except models.DoesNotExist:
+                flash('Email or password does not match, please try again', 'error')
+        else:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash('You are logged in!', 'success')
+                return redirect(url_for('index'))
+            else:
+                flash('Email or password does not match, please try again', 'error')
+    return render_template('login.html', form=form)
+
+ @app.route('/logout')      
+ @login_required
+ def logout():
+     logout_user()
+     flash('You logged out successfully.', 'success')
+     return redirect(url_for('index')) 
+
 
 @app.route('/')
 @app.route('/entries')
-def listing():
+def index():
+    stream = models.JournalList.select().limit(25).order_by(models.Entry.date.desc())
+    render_template('index.html', stream=stream)
 
 
 @app.route('/entries/new', methods=('GET', 'POST'))
 def create():
+    pass
 
 
 @app.route('/entries/<id>', methods=('GET', 'POST'))
 def detail(id):
+    pass
 
 
 @app.route('/entries/<id>/edit', methods=('GET', 'POST'))
 def edit(id):
+    pass
 
 
 @app.route('/entries/<id>/delete', methods=('GET', 'POST'))
 def delete(id):
+    pass
 
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    models.initialize()
+    try:
+        models.User.create_user(
+            username='danig',
+            email='dfleharty@gmail.com',
+            password='python',
+            admin=True
+        )
+    except ValueError:
+        pass
+    try:
+        models.Entry.create_entry(
+            title='Hello', date='date', 
+            time_spent=5, learned='python', 
+            resources='python'
+            )
+    except ValueError:
+        pass
+    app.run(debug=True, port=8000, host='localhost')
 
--- models.py
+++ models.py
@@ -1,36 +1,91 @@
 import datetime
 
+from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash
+
 from peewee import *
 
 
 db = SqliteDatabase('journal.db')
 
 
-class Entries(Model):
-    title = CharField(max_length=250)
-    date = DateTimeField(default=datetime.datetime.now)
-    time_spent = IntegerField()
-    learned = TextField()
-    resources = TextField()
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    joined_at = DateTimeField(datetime.datetime.now)
+    is_admin = BooleanField(default=False)
 
     class Meta:
         database = db
         order_by = ('-joined_at',)
-
+    
+    def get_entries(self):
+        return Entry.select().where(Entry.user == self)
+        
     @classmethod
-    def create_entry(cls, title, date, time_spent, learned, resources):
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            with models.db.transaction():
+                cls.create(
+                    username=username,
+                    email=email,
+                    password=generate_password_hash(password),
+                    is_admin=admin 
+                )
+        except IntegrityError:
+            raise ValueError('User already exists.')
 
-class Tag(Model):
-    one = ForeignKeyField()
-    many = ForeignKeyField()
+
+class Entry(Model):
+    title = CharField(max_length=225)
+    entry_date = DateTimeField(default=datetime.date.today)
+    time_spent = IntegerField(null=False)
+    learned = TextField()
+    resources = TextField()
+    user = ForeignKeyField(User, related_name='entries')
+    timestamp = DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         database = db
-        indexes = ()
-    
+        order_by = ('-entry_date',)
 
+    @classmethod
+    def create_entry(cls, title, entry_date, time_spent, learned, resources, user,):
+        try:
+            cls.create(
+                title=title, 
+                entry_date=date,
+                time_spent=time_spent,
+                learned=learned,
+                resources=resources, 
+                user=user
+                )
+        except IntegrityError:
+            raise ValueError('Entry already exists')
+
+
+class JournalList(Model):
+    title = CharField(max_length=250)
+    date = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = db
+        order_by = ('-timestamp',)
+
+
+class Tag(Model):
+    one = ForeignKeyField(User, related_name='relationships')
+    many = ForeignKeyField(User, related_name='related_to')
+
+    class Meta:
+        database = db
+        indexes = (
+            (('one', 'many'), True),
+        )
+    
 
 def initialize():
     db.connect()
-    db.create_tables([Entries], safe=True)
+    db.create_tables([User, Entry, JournalList, Tag], safe=True)
     db.close()
